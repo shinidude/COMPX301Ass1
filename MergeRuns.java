b468add027bbc7c12eddb206cf4e30720398a0c4
Joethy <61763778+Joethy@users.noreply.github.com>
2023-04-02 19:55:15 +1200

    mergeruns is proper 2 way now
    It no longer takes an input (potentially greater than 2), distributes it based on that, then only merges 2 files (0 and 1)

--- MergeRuns.java
+++ MergeRuns.java
@@ -3,13 +3,7 @@ import java.io.*;
 
 public class MergeRuns {
     public static void main(String[] args) {
-        DistributeRuns db;
-        try {
-            db = new DistributeRuns(Integer.parseInt(args[0]));
-        } catch (Exception e) {
-            e.printStackTrace();
-            db = new DistributeRuns(2);
-        }
+        DistributeRuns db = new DistributeRuns(2);
         db.DR();
         
         try {
